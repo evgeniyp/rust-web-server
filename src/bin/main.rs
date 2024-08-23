cbdd3e582738ae09dbdb14f01a4d3a4bfdaad65b
Evgeniy Petukhov <evgeniy.petukhov@gmail.com>
2024-08-23 14:57:16 +0200

    Implemented a thread pool

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -2,14 +2,21 @@ use std::fs;
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
+use std::thread;
+
+use rust_web_server::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("0.0.0.0:7878").unwrap();
 
+    let pool = ThreadPool::new(4);
+
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
 
@@ -18,9 +25,15 @@ fn handle_connection(mut stream: TcpStream) {
     stream.read(&mut buffer).unwrap();
 
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
+
     let (status_line, filename) = if buffer.starts_with(get) {
         ("HTTP/1.1 200 OK", "index.html")
-    } else {
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(std::time::Duration::from_secs(5));
+        ("HTTP/1.1 200 OK", "index.html")
+    }
+    else {
         ("HTTP/1.1 404 NOT FOUND", "404.html")
     };
 
